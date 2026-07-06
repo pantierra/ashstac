3d31af7a372991e2d24b837a7991c94582f93422
Felix Delattre <felix@developmentseed.org>
2026-07-06 22:51:27 +0200

    chore: added release-please.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule AshStac.MixProject do
   def project do
     [
       app: :ash_stac,
-      version: "0.1.0",
+      version: "0.0.1",
       elixir: "~> 1.20",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
